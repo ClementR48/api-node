a88483b1b4698d5031230e85bcb6b5871df6aa37
Clement <riera.clement@gmail.com>
2022-06-07 17:26:34 +0200

    some fix

--- src/routes/deletePokemon.js
+++ src/routes/deletePokemon.js
@@ -1,26 +1,24 @@
-const { Pokemon } = require("../db/sequelize");
+const { Pokemon } = require('../db/sequelize')
+const auth = require('../auth/auth')
 
 module.exports = (app) => {
-  app.delete("/api/pokemons/:id", (req, res) => {
-    Pokemon.findByPk(req.params.id).then((pokemon) => {
-      const pokemonDeleted = pokemon;
-      return Pokemon.destroy({
-        where: { id: pokemon.id },
-      })
-        .then((_) => {
-          if (pokemon === null) {
-            const message =
-              "Le pokémon demandé n'existe pas. Réessayez avec un autre identifiant";
-            res.status(404).json({ message });
-          }
-          const message = `Le pokémon avec l'identifiant n°${pokemonDeleted.id} a bien été supprimé.`;
-          res.json({ message, data: pokemonDeleted });
+  app.delete('/api/pokemons/:id', auth, (req, res) => {
+    Pokemon.findByPk(req.params.id)
+      .then(pokemon => {        
+        if(pokemon === null) {
+          const message = `Le pokémon demandé n'existe pas. Réessayez avec un autre identifiant.`
+          return res.status(404).json({ message })
+        }
+
+        return Pokemon.destroy({ where: { id: pokemon.id } })
+        .then(_ => {
+          const message = `Le pokémon avec l'identifiant n°${pokemon.id} a bien été supprimé.`
+          res.json({message, data: pokemon })
         })
-        .catch((error) => {
-          const message =
-            "Le pokemon n'a pas pu être modifié. Réessayez dans quelques instants.";
-          res.status(500).json({ message, data: error });
-        });
-    });
-  });
-};
+      })
+      .catch(error => {
+        const message = `Le pokémon n'a pas pu être supprimé. Réessayez dans quelques instants.`
+        res.status(500).json({ message, data: error })
+      })
+  })
+}
