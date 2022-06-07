f6dca14c644f78a934fb4371ccdffffd75891a21
Clement <riera.clement@gmail.com>
2022-06-07 19:08:24 +0200

    tac

--- src/routes/login.js
+++ src/routes/login.js
@@ -7,8 +7,8 @@ module.exports = (app) => {
   app.post('/api/login', (req, res) => {
 
     User.findOne({ where: { username: req.body.username } }).then(user => {
-
-      if(!user) {
+      console.log(user);
+      /* if(!user) {
         const message = `L'utilisateur demandé n'existe pas.`
         return res.status(404).json({ message })
       }
@@ -28,7 +28,7 @@ module.exports = (app) => {
 
         const message = `L'utilisateur a été connecté avec succès`;
         return res.json({ message, data: user, token })
-      })
+      }) */
     })
     .catch(error => {
       const message = `L'utilisateur n'a pas pu être connecté. Réessayez dans quelques instants.`
